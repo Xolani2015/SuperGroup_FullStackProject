4ba1e555b4ee13e49f6d1a9fe6155b6d01370008
Xolani <xr.mkhwanazi.20@gmail.com>
2023-07-23 03:28:56 -0700

    create orders table

--- super_group_front_end/src/pages/orders/orders.js
+++ super_group_front_end/src/pages/orders/orders.js
@@ -1,12 +1,49 @@
 import React from 'react';
 import './orders.css'
+import { ForwardIcon  } from  '../../components/forward_icon';
 
 export const Orders = () => {
+
+    const data = [
+        { id: 1, text1: 'First Text', image: 'path/to/image.jpg', text2: 'Second Text', text3: 'Third Text' },
+        // Add more data rows as needed
+      ];
   return (
     <div className="orders">
       <h1>Your orders</h1>
-      <h1>You have no recent orders</h1>
-
+       <div className="custom-table-container">
+      <table className="custom-table">
+        <thead>
+          <tr>
+            <th>PRODUCT</th>
+            <th>QUATITIY</th>
+            <th>TOTAL</th>
+            <th>ITEMS</th>
+        
+          </tr>
+        </thead>
+        <tbody>
+          {data.map((item) => (
+            <React.Fragment key={item.id}>
+              <tr>
+              <td>
+                  <img src={item.image} alt="Image" className="table-image" />
+                </td>
+                <td>{item.text1}</td>
+               
+                <td>{item.text2}</td>
+    
+                <td>
+                <div className="circle-container">
+        <ForwardIcon />
+      </div>
+                </td>
+              </tr>
+            </React.Fragment>
+          ))}
+        </tbody>
+      </table>
+    </div>
     </div>
   );
 };
