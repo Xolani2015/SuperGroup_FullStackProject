568ad2e306e8944baafc796fdd22dae53e0f2e0a
Xolani <xr.mkhwanazi.20@gmail.com>
2023-07-23 03:28:20 -0700

    Add for orders foward ui

---
+++ super_group_front_end/src/components/forward_icon.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export const ForwardIcon = () => (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="24"
+      height="24"
+      viewBox="0 0 24 24"
+      fill="none"
+      stroke="currentColor"
+      strokeWidth="2"
+      strokeLinecap="round"
+      strokeLinejoin="round"
+      className="forward-icon"
+    >
+      <path d="M5 12h14M12 5l7 7-7 7" />
+    </svg>
+  );
+
+
